548560b96bd81cc06bed52d46b0886c1ec8adb47
Luca <lucaveronese99@gmail.com>
2021-08-12 00:46:53 +0200

    Changed template

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cat doc_info.md lesson01.md lesson02.md lesson03.md lesson04.md lesson05.md lesson06.md lesson07.md lesson08.md lesson09.md lesson10.md lesson11.md lesson12.md lesson13.md lesson14.md lesson15.md lesson16.md lesson17.md lesson18.md lesson19.md lesson20.md lesson21.md lesson22.md lesson23.md lesson24.md > tmp_book.md 
 sed -i 's/svg/png/g' tmp_book.md
-pandoc "tmp_book.md" -o "tmp_book.tex" --from markdown --template "../pandoc-latex-template/eisvogel.tex" --listings
+# pandoc "tmp_book.md" -o "tmp_book.tex" --from markdown --template "https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex" --listings
+pandoc "tmp_book.md" -o "tmp_book.tex" --from markdown --template "https://raw.githubusercontent.com/oehrlis/pandoc_template/master/templates/trivadis.tex" --listings
 latexmk -pdf -f -interaction=nonstopmode tmp_book.tex -jobname="Appunti-APP"
 #clean house
 rm -f *.aux *.ext *.idx *.ilg *.ind *.log *.lot *.lof *.tmp *.out *.glo *.gls *.fls *.fdb* *.toc *.xtr
--- doc_info.md
+++ doc_info.md
@@ -1,15 +1,20 @@
 ---
 title: "Appunti di Altri Paradigmi di Programmazione"
+subtitle: "Versione libro delle slides del corso di A.P.P., non ufficiale."
 author: [Luca Veronese]
 date: ""
 subject: "Altri Paradigmi di Programmazione"
 keywords: [Java, Programmazione Concorrente, Programmazione Distribuita]
-subtitle: "Versione libro delle slides del corso di A.P.P.,  non ufficiale."
 lang: "it"
 titlepage: true
 toc: true
 toc-own-page: true
-header-left: "\\hspace{1cm}"
-footer-left: "\\sectionmark"
-footer-right: "\\thepage"
+# header-left: "\\hspace{1cm}"
+header-left: "\\rightmark"
+# footer-right: "\\thepage"
+papersize: a4 
+listings-disable-line-numbers: true
+numbersections: true
+mainfont: Nunito Sans SemiBold
+monofont: Courier New
 ...
